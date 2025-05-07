bce688307df213eb1562c46f6b8b9decdc6729e4
Hajira Baffoe <145415237+hajirabbb@users.noreply.github.com>
2025-05-07 08:19:11 +0000

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,29 @@
+/*#include <iostream>
+using namespace std;
+int num1, num2;
+
+
+
+int main() {
+    cout <<"please input first number";
+    cin >> num1;
+   cout<<"please input second number";
+    cin >> num2;
+    cout<< "result" << num1 + num2 <<std::endl;
+    return 0;
+}*/
+#include <iostream>
+using namespace std;
+
+int FirstNumber, SecondNumber;
+
+int main() {
+    cout << "Please input first number: ";
+    cin >> FirstNumber;
+    
+    cout << "Please input second number: ";
+    cin >> SecondNumber;
+    
+    cout << "Sum: " << FirstNumber + SecondNumber << endl;
+    return 0;
+}
